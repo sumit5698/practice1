fbf4a1c68226e9441cd317f8597448f1ecae1cb3
sumit5698 <asok356000@gmail.com>
2026-01-22 06:46:31 +0000

    variavle or datatypes

---
+++ .devcontainer/02_datatypes.js
@@ -0,0 +1,13 @@
+let name= "sumit"
+let age = 19
+let islogging = false
+
+//number =>2 to power 53
+//bigint
+//string => true/false
+//null=  standalone value
+//undefined=>  
+//symbol=>  unique
+//object
+
+console.log()
---
+++ .devcontainer/variables.js
@@ -0,0 +1,16 @@
+const accountId = 12344
+let accountName = "sumit"
+ city = "jaipur"
+let accountEmail ="sumit@1123"
+let accountstate
+
+console.log(accountEmail);
+
+
+/*
+prefer to not use 
+var to not use
+because of issue in block scope and fuction scope
+*/
+
+console.table([accountId,accountEmail,city,accountName,accountstate]);
